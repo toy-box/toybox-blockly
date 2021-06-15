f7d3bc3668a5e4a256132e2ee0b75acf27a3ff57
Huhui <wishgood@gmail.com>
2021-06-15 13:44:44 +0800

    完善

--- src/registry.ts
+++ src/registry.ts
@@ -1,15 +1,15 @@
 import Blockly from 'blockly';
 import 'blockly/javascript';
 
-const ArgRex = /^args[0-9]\d*$/
+const ArgRex = /^args[0-9]\d*$/;
+
 export const blockRegistry = (name: string, json: Record<string, any>, code: string) => {
-  Blockly.Blocks[name] = {
-    init: function() {
-      (this as Blockly.Block).jsonInit(json)
-    }
-  };
+  Blockly.defineBlocksWithJsonArray([{
+    ...json,
+    type: json.type || name
+  }]);
   (Blockly as any).JavaScript[name] = (block: Blockly.Block) => {
-    const args = [] as Array<{ name: string, value: any}>
+    const args = [] as Array<{ name: string, value: any}>;
     Object.keys(json)
       .filter(key => ArgRex.test(key))
       .forEach(argsKey => {
@@ -19,9 +19,8 @@ export const blockRegistry = (name: string, json: Record<string, any>, code: str
             value: block.getFieldValue(arg.type)
           }))
         )
-      })
-    console.log('args', args)
-    const fn = new Function(...args.map(arg => arg.name), `return ${code}`)
+      });
+    const fn = new Function(...args.map(arg => arg.name), `return ${code}`);
     return [fn(...args.map(arg => arg.value)), (Blockly as any).JavaScript.ORDER_NONE];
   }
 }
